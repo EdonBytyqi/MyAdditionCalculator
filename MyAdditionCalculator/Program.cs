7d9268876a7ca00f1dd80dea1869ebf61183d567
Edon Bytyqi <edonbytyqi@outlook.com>
2025-05-28 11:12:06 +0200

    added double

--- MyAdditionCalculator/Program.cs
+++ MyAdditionCalculator/Program.cs
@@ -1,23 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // datatype variableName = initial value;
-int myNumber = 0;
-int myNumber2 = 0;
+double myNumber1 = 0;
+double myNumber2 = 0;
 
 
 // Prints out whatever is inside of ();
-Console.WriteLine("Enter a whole number");
+Console.WriteLine("Enter a number");
 
 // takes the user input and stores it
 // Variable with the name "userInpunt" and the data type "string"
 string userInput = Console.ReadLine();
 //myNumber = int.Parse(Console.ReadLine());
-myNumber = int.Parse(userInput);
+myNumber1 = double.Parse(userInput);
 
-Console.WriteLine("Enter a whole number");
+Console.WriteLine("Enter a number");
 userInput = Console.ReadLine();
-myNumber2 = int.Parse(userInput);
+myNumber2 = double.Parse(userInput);
 
 //we are adding to strings together and writing them onto the console
-int sum = myNumber + myNumber2;
-Console.WriteLine("The sum is: " + sum);
+double sum = myNumber1 + myNumber2;
+Console.WriteLine($"The sum is: {sum}");
 Console.ReadKey();
